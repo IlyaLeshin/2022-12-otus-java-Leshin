4c88fdf81fe6de0815288f6f24bf72e4e5282008
IlyaLeshin <109245793+IlyaLeshin@users.noreply.github.com>
2022-12-14 00:41:16 +0300

    hw02-collections (ДЗ: Применение коллекций) (#2)

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 Группа: 2022-09  
 
 ## Home works
-- [x] hw01-gradle
+- [x] hw01-gradle (ДЗ: Проект gradle с модульной структурой)
+- [x] hw02-collections (ДЗ: Применение коллекций)
---
+++ hw02-collections/src/main/java/ru.otus.homeworks.hw2/CustomerService.java
@@ -0,0 +1,38 @@
+package ru.otus.homeworks.hw2;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class CustomerService {
+
+    private final NavigableMap<Customer, String> customerTreeMap = new TreeMap<>();
+
+    public Map.Entry<Customer, String> getSmallest() {
+        Customer smallestCustomer = customerTreeMap.firstEntry().getKey();
+        String smallestCustomersData = customerTreeMap.get(smallestCustomer);
+        return returnEntry(smallestCustomer, smallestCustomersData);
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        try {
+            Customer nextCustomer = customerTreeMap.higherKey(customer);
+            String nextCustomersData = customerTreeMap.get(nextCustomer);
+            return returnEntry(nextCustomer, nextCustomersData);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    public void add(Customer customer, String data) {
+        customerTreeMap.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> returnEntry(Customer customer, String data) {
+        return Map.entry(new Customer(
+                        customer.getId(),
+                        customer.getName(),
+                        customer.getScores()),
+                data);
+    }
+}
