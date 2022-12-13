3cd8c284d9ed02bd36327c5b7c040402e3473dd4
IlyaLeshin <ilya139@list.ru>
2022-12-14 00:38:01 +0300

    hw03-annotations (ДЗ: Свой тестовый фреймворк)

---
+++ hw03-annotations/src/main/java/ru.otus.homeworks.hw3/Main.java
@@ -0,0 +1,7 @@
+package ru.otus.homeworks.hw3;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException{
+        TestRunner.runTests("ru.otus.homeworks.hw3.TestClass");
+        }
+}
