28c16e015a6f7363df86b1deb7c8bbdbf3a0445e
IlyaLeshin <ilya139@list.ru>
2022-12-15 01:00:50 +0300

    hw04-GarbageCollector (ДЗ: Определение нужного размера хипа)

---
+++ hw04-garbage_collector/src/main/java/ru/otus/homeworks/hw4/calculator/Data.java
@@ -0,0 +1,13 @@
+package ru.otus.homeworks.hw4.calculator;
+
+public class Data {
+    private final int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
