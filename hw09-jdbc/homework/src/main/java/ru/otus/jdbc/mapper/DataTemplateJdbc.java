1a0b6d44a5675776737f762065b57b5f0368a0ad
IlyaLeshin <ilya139@list.ru>
2023-04-17 07:54:51 +0300

    hw09-jdbc (ДЗ: Самодельный ORM)
    homework is done

--- hw09-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
+++ hw09-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -1,42 +1,109 @@
 package ru.otus.jdbc.mapper;
 
 import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.repository.DataTemplateException;
 import ru.otus.core.repository.executor.DbExecutor;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
-import java.util.List;
-import java.util.Optional;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 /**
- * Сохратяет объект в базу, читает объект из базы
+ * Сохраняет объект в базу, читает объект из базы
  */
 public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     private final DbExecutor dbExecutor;
     private final EntitySQLMetaData entitySQLMetaData;
+    private final EntityClassMetaData<T> entityClassMetaData;
 
-    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
+    public DataTemplateJdbc(DbExecutor dbExecutor,
+                            EntitySQLMetaData entitySQLMetaData,
+                            EntityClassMetaData<T> entityClassMetaData) {
         this.dbExecutor = dbExecutor;
         this.entitySQLMetaData = entitySQLMetaData;
+        this.entityClassMetaData = entityClassMetaData;
     }
 
     @Override
     public Optional<T> findById(Connection connection, long id) {
-        throw new UnsupportedOperationException();
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectByIdSql(), List.of(id), resultSet -> {
+            try {
+                if (resultSet.next()) {
+                    return createAnInstance(resultSet);
+                }
+                return null;
+            } catch (SQLException e) {
+                throw new DataTemplateException(e);
+            }
+        });
     }
 
     @Override
     public List<T> findAll(Connection connection) {
-        throw new UnsupportedOperationException();
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectAllSql(), Collections.emptyList(), resultSet -> {
+            try {
+                List<T> list = new ArrayList<>();
+                if (resultSet.next()) {
+                    var field = createAnInstance(resultSet);
+                    list.add(field);
+                }
+                return list;
+            } catch (SQLException e) {
+                throw new DataTemplateException(e);
+            }
+        }).orElseThrow();
     }
 
     @Override
     public long insert(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        return dbExecutor.executeStatement(connection, entitySQLMetaData.getInsertSql(),
+                entityClassMetaData.getFieldsWithoutId()
+                        .stream()
+                        .map(field -> {
+                            field.setAccessible(true);
+                            try {
+                                return field.get(client);
+                            } catch (IllegalAccessException e) {
+                                throw new DataTemplateException(e);
+                            }
+                        }).toList());
     }
 
     @Override
     public void update(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        dbExecutor.executeStatement(connection, entitySQLMetaData.getUpdateSql(),
+                entityClassMetaData
+                        .getFieldsWithoutId()
+                        .stream()
+                        .map(field -> {
+                            field.setAccessible(true);
+                            try {
+                                return field.get(client);
+                            } catch (IllegalAccessException e) {
+                                throw new DataTemplateException(e);
+                            }
+                        })
+                        .toList());
+    }
+
+    private T createAnInstance(ResultSet resultSet) {
+        try {
+            T instance = entityClassMetaData.getConstructor().newInstance();
+            entityClassMetaData.getAllFields().forEach(field ->
+            {
+                try {
+                    field.setAccessible(true);
+                    field.set(instance, resultSet.getObject(field.getName()));
+                } catch (IllegalAccessException | SQLException e) {
+                    throw new DataTemplateException(e);
+                }
+            });
+            return instance;
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new DataTemplateException(e);
+        }
     }
 }
